3fdda4f225ce6152fb4f2201825be03df463a05d
kalle bertell <carlpetter.bertell@gmail.com>
2020-11-05 12:53:25 +0100

    fix settings

--- djangowind/wsgi.py
+++ djangowind/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangowinds.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangowind.settings')
 
 application = get_wsgi_application()
