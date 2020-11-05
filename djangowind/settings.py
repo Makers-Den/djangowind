f6a6934d6951bba14ff1070b10cf38570d0d0d71
kalle bertell <carlpetter.bertell@gmail.com>
2020-11-05 12:43:34 +0100

    heroku reqs

--- djangowind/settings.py
+++ djangowind/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Configure Django App for Heroku.
+django_heroku.settings(locals())
